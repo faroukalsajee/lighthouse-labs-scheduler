7511041d492ff9dcbb54c06bd0cef88ed6a3c7b1
faroukalsajee <faroukalsajee@gmail.com>
2021-04-21 18:13:24 -0700

    fixed save buton bug

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,119 +1,128 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import Header from "./Header";
-import Show from "./Show";
-import Empty from "./Empty";
-import Form from "./Form";
-import Status from "./Status";
-import Confirm from "./Confirm";
-import Error from "./Error";
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import Status from 'components/Appointment/Status';
+import Confirm from 'components/Appointment/Confirm';
+import Error from 'components/Appointment/Error';
 
-import "components/Appointment/styles.scss";
-import useVisualMode from "hooks/useVisualMode";
-
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-const CREATE = "CREATE";
-const SAVING = "SAVING";
-const DELETING = "DELETING";
-const CONFIRM = "CONFIRM";
-const EDIT = "EDIT";
-const ERROR_SAVE = "ERROR_SAVE";
-const ERROR_DELETE = "ERROR_DELETE";
+import 'components/Appointment/styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
+
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
-  //Handles Appointment component transitions
   useEffect(() => {
+    
     if (props.interview && mode === EMPTY) {
       transition(SHOW);
     }
-    if (props.interview === null && mode === SHOW) {
+    
+    if (!props.interview && mode === SHOW) {
       transition(EMPTY);
     }
-  }, [props.interview, transition, mode]);
 
-  // Functions for saving and deleting Appointments
+  }, [mode, transition, props.interview])
+
   function save(name, interviewer) {
-    const interview = {
-      student: name,
-      interviewer,
-    };
-    transition(SAVING);
-    props
-      .bookInterview(props.id, interview)
-      .then(() => transition(SHOW))
-      .catch((err) => {
-        console.error(`An error occured while attempting to save: ${err}`);
-        transition(ERROR_SAVE, true);
-      });
-  }
-  // removes an appointment
-  function remove() {
-    if (mode === CONFIRM) {
-      transition(DELETING, true);
-      props
-        .cancelInterview(props.id)
-        .then(() => transition(EMPTY))
-        .catch(() => transition(ERROR_DELETE, true));
-    } else {
-      transition(CONFIRM);
+    
+    if (name && interviewer) {
+      transition(SAVING);
+
+      const interview = {
+        student: name,
+        interviewer
+      };
+
+      props.bookInterview(props.id, interview)
+        .then(() => transition(SHOW))
+        .catch(() => transition(ERROR_SAVE, true))
     }
   }
 
-  // Appointment component handles transitions between modes
+  function remove() {
+
+    if (mode === CONFIRM) {
+      transition(DELETING, true)
+      props.cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE, true))
+    } else {
+      transition(CONFIRM);      
+    }
+  }
+
+  function edit() {
+    transition(EDIT);
+  }
+  
   return (
+
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === CREATE && (
-        <Form
-          interviewers={props.interviewers}
-          onSave={save}
-          onCancel={() => back()}
-        />
-      )}
-      {mode === SHOW && props.interview && (
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onEdit={() => transition(EDIT)}
-          onDelete={() => transition(CONFIRM)}
+          onDelete={remove}
+          onEdit={edit}
         />
       )}
-      {mode === EDIT && (
+      {mode === CREATE &&
         <Form
-          name={props.interview.student}
-          interviewer={props.interview.interviewer.id}
+          name={props.name}
+          value={props.value}
           interviewers={props.interviewers}
           onSave={save}
-          onCancel={() => transition(SHOW)}
-        />
-      )}
+          onCancel={back}
+        />}
       {mode === SAVING && <Status message="Saving" />}
-      {mode === ERROR_SAVE && (
-        <Error
-          message="An error occured while attempting to save appointment."
-          onClose={() => back()}
-        />
-      )}
-      {mode === CONFIRM && (
-        <Confirm
-          message="Are you sure you would like to delete?"
-          onConfirm={remove}
-          onCancel={() => transition(SHOW)}
-        />
-      )}
       {mode === DELETING && <Status message="Deleting" />}
-      {mode === ERROR_DELETE && (
-        <Error
-          message="An error occured while attempting to delete appointment."
-          onClose={() => back()}
+      {mode === CONFIRM && 
+        <Confirm 
+          onCancel={back}
+          onConfirm={remove}
+          message="Are you sure you would like to delete?" 
+        />}
+      {mode === EDIT &&
+        <Form 
+          name={props.name ? props.name : props.interview.student}
+          value={props.value ? props.value: props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={back}
+          
         />
-      )}
+      }
+      {mode === ERROR_SAVE && 
+        <Error 
+          message="Could not create appointment"
+          onClose={back}
+        />
+      }
+      {mode === ERROR_DELETE && 
+        <Error 
+          message="Could not cancel appointment"
+          onClose={back}
+        />
+      }
     </article>
-  );
+
+  )
 }
